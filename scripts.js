ecef44feba74061a5721c51e50d0c8da1db1a9a8
yagovysk <yago09regis@gmail.com>
2025-04-18 13:28:13 -0300

    adicionando responsividade mobile e outras funcionalidades

--- scripts.js
+++ scripts.js
@@ -1,7 +1,26 @@
-const menuToggle = document.querySelector(".menu-toggle");
-const menuContainer = document.querySelector(".menu-container");
+document.addEventListener("DOMContentLoaded", () => {
+  const menuToggle = document.querySelector(".menu-toggle");
+  const menuClose = document.querySelector(".menu-close");
+  const menuContainer = document.querySelector(".menu-container");
+  const menuLinks = document.querySelectorAll(".menu a");
 
-menuToggle.addEventListener("click", () => {
-  menuContainer.classList.toggle("active"); // Expande ou recolhe o menu
-  menuToggle.classList.toggle("active"); // Alterna o estado do ícone
+  // Verificação preventiva
+  if (menuToggle && menuClose && menuContainer) {
+    // Abrir o menu
+    menuToggle.addEventListener("click", () => {
+      menuContainer.classList.add("active");
+    });
+
+    // Fechar o menu
+    menuClose.addEventListener("click", () => {
+      menuContainer.classList.remove("active");
+    });
+
+    // Fechar ao clicar em um link do menu
+    menuLinks.forEach((link) => {
+      link.addEventListener("click", () => {
+        menuContainer.classList.remove("active");
+      });
+    });
+  }
 });
